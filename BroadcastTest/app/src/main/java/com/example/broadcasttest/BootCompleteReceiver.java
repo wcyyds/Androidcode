6ba173c71c1a97276757bb29922afa59badb0ddc
wcyyds <2077364427@qq.com>
2022-08-02 15:55:09 +0800

    zidingyiguangbo

---
+++ BroadcastTest/app/src/main/java/com/example/broadcasttest/BootCompleteReceiver.java
@@ -0,0 +1,14 @@
+package com.example.broadcasttest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class BootCompleteReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Boot Complete,这个是开机广播", Toast.LENGTH_SHORT).show();
+    }
+}
