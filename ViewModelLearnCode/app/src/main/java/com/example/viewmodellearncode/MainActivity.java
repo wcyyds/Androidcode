8b14be5c0523f45d99e9c0af1a45aa8c2b2abaee
wcyyds <2077364427@qq.com>
2023-07-31 18:29:30 +0800

    viewmodel demo source Learn

--- ViewModelLearnCode/app/src/main/java/com/example/viewmodellearncode/MainActivity.java
+++ ViewModelLearnCode/app/src/main/java/com/example/viewmodellearncode/MainActivity.java
@@ -1,14 +1,67 @@
 package com.example.viewmodellearncode;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.example.viewmodellearncode.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    @SuppressLint({"MissingInflatedId", "LocalSuppress"})
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        Log.i("111", "onCreate: ");
+
+        TextView tvUserName = findViewById(R.id.textView);
+        ProgressBar pbLoading = findViewById(R.id.pb_loading);
+        //获取ViewModel实例
+        ViewModelProvider viewModelProvider = new ViewModelProvider(this);
+        UserViewModel userViewModel = viewModelProvider.get(UserViewModel.class);
+        //观察 用户信息
+        userViewModel.getUserLiveData().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                // update ui.
+                tvUserName.setText(s);
+            }
+        });
+
+        userViewModel.getLoadingLiveData().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean aBoolean) {
+                pbLoading.setVisibility(aBoolean? View.VISIBLE:View.GONE);
+            }
+        });
+        //点击按钮获取用户信息
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userViewModel.getUserInfo();
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("111", "onStop: ");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("111", "onDestroy: ");
     }
 }
---
+++ ViewModelLearnCode/app/src/main/java/com/example/viewmodellearncode/UserViewModel.java
@@ -0,0 +1,50 @@
+package com.example.viewmodellearncode;
+
+import android.os.AsyncTask;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class UserViewModel extends ViewModel {
+
+    private MutableLiveData<String> userLiveData ;
+    private MutableLiveData<Boolean> loadingLiveData;
+
+    public UserViewModel() {
+        userLiveData = new MutableLiveData<>();
+        loadingLiveData = new MutableLiveData<>();
+    }
+
+    //获取用户信息,假装网络请求 2s后 返回用户信息
+    public void getUserInfo() {
+
+        loadingLiveData.setValue(true);
+
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected void onPostExecute(String s) {
+                loadingLiveData.setValue(false);
+                userLiveData.setValue(s);//抛出用户信息
+            }
+            @Override
+            protected String doInBackground(Void... voids) {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                String userName = "nihao";
+                return userName;
+            }
+        }.execute();
+    }
+
+    public LiveData<String> getUserLiveData() {
+        return userLiveData;
+    }
+    public LiveData<Boolean> getLoadingLiveData() {
+        return loadingLiveData;
+    }
+}
+
